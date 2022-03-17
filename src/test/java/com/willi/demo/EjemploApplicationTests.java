2a18b259b05265e1ce9d883c5d10b7f6cc5e0cbe
Gerónimo Corti <geronimo.corti@despegar.com>
2022-03-17 17:35:20 -0300

    Spring boot

---
+++ src/test/java/com/willi/demo/EjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
